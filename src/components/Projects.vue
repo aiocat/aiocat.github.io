740d562396c49c8effc281c37feef84b7400af1e
aiocat <aiocat@protonmail.com>
2022-06-11 13:43:39 +0300

    add ripgvc

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -23,11 +23,12 @@ import ProjectCard from './ProjectCard.vue'
             <ProjectCard mark="" title="Pixasso" desc="Reddit place like website written in one day" repo="pixasso" prog="Go" />
             <ProjectCard mark="" title="Webcord" desc="Chrome extension to use themes in discord web" repo="webcord" prog="JS" />
             <ProjectCard mark="" title="JDW" desc="Dynamic windows library for vanilla-js" repo="jdw" prog="JS" />
-            <ProjectCard mark="gold" title="Jel" desc="Flexible, memory-safe, easy-to-use, interpreted programming language" repo="jel" prog="Rust" />
+            <ProjectCard mark="gold" title="RipGVC" desc="GitHub profile visitor counter" repo="ripgvc" prog="Rust" />
+            <ProjectCard mark="" title="Jel" desc="Flexible, memory-safe, easy-to-use, interpreted programming language" repo="jel" prog="Rust" />
             <ProjectCard mark="gold" title="Taixoxo" desc="osu!taiko bot for Windows" repo="taixoxo" prog="Rust" />
+            <ProjectCard mark="" title="Axis" desc="CLI tool for find text from files" repo="axis" prog="Rust" />
             <ProjectCard mark="gold" title="DLL Crab" desc="Rusty DLL Injector with GUI" repo="dll-crab" prog="Rust" />
             <ProjectCard mark="gold" title="Request" desc="Minimal HTTP client with Vue3 front-end" repo="request" prog="Rust" />
-            <ProjectCard mark="" title="Axis" desc="CLI tool for find text from files" repo="axis" prog="Rust" />
             <ProjectCard mark="" title="Solute" desc="Github-like avatar creator library" repo="solute" prog="Rust" />
             <ProjectCard mark="" title="Totify" desc="Lightweight notification creator for browser" repo="totift" prog="TS" />
         </div>
